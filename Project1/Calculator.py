0cb5190620ede626e0c736b964ad0b69613573fd
vonglacl <143111008+vonglacl@users.noreply.github.com>
2023-10-09 17:29:08 -0400

    Create Calculator.py
    I used information from: https://www.digitalocean.com/community/tutorials/how-to-make-a-calculator-program-in-python-3 to create this script.
    
    Once this script is run, the user will be able to select an operation of either:
    1. Addition
    2. Subtraction
    3. Multiplication
    4. Division
    The user then enters either 1, 2, 3, or 4.
    For this example, let's say the user entered the number 2 for subtraction.
    
    Once the operation is chosen, the user can enter their first number. For this example, we will use the number 13.
    
    The user can then enter a second number, which will be the number that is subtracted from 13. For this example, we will use the number 5.
    
    The code then inputs the answer to 13 - 5, which is 8.0.
    
    Lastly, the code will ask the user if they would like to perform another calculation, while the user can either answer with "yes" or with "no."
    
    If the user answers with "yes," the entire code will start again and the user will start at the beginning where they can select an operation of 1, 2, 3, or 4.
    
    If the user answers with "no," then the coding will end.

---
+++ Project1/Calculator.py
@@ -0,0 +1,43 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    else:
+        return a / b
+
+print("Simple Calculator")
+print("Select Operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+while True:
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print("Result:", add(num1, num2))
+        elif choice == '2':
+            print("Result:", subtract(num1, num2))
+        elif choice == '3':
+            print("Result:", multiply(num1, num2))
+        elif choice == '4':
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid input")
+
+    next_calculation = input("Do you want to perform another calculation? (yes/no): ")
+    if next_calculation.lower() != 'yes':
+        break
